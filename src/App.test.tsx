175b3d3abd8395dbf6d26188b3ee77840fb5d164
troias <hunter007nz@gmail.com>
2023-08-28 18:55:52 +1200

    added check/state events

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,68 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
-
-
+import { log } from "console";
 
 //test if hello world in page
 
-// Button has bg-blue500 intial color 
+// Button has bg-blue500 intial color
 test("Button has bg-blue500 initial color", () => {
   render(<App />);
   const buttonElement = screen.getByRole("button");
   expect(buttonElement).toHaveClass("bg-blue-500");
 });
 
+// get state that if button is clicked and state is false then bg-red-500
 
-// Button has correct intial text
+test("Button click toggles state, is blue intially then red", () => {
+  const { getByText } = render(<App />);
+  const button = getByText("Test Button");
 
-//Button turns Red when clicked
+  // Initially, the state should be true
+  expect(button).toHaveClass("bg-blue-500");
 
+  // Simulate a button click
+  fireEvent.click(button);
+
+  // After the click, the state should change, so the class should change
+  expect(button).toHaveClass("bg-red-500");
+
+  // Test if the button text changes
+  expect(button).toHaveTextContent("Test Button");
+});
+
+//expect color button to be enabled
+
+test("Button is enabled", () => {
+  const { getByText } = render(<App />);
+  const button = getByText("Test Button");
+  expect(button).toBeEnabled();
+});
+
+//expect check box is unchecked intially
+
+test("Checkbox is unchecked", () => {
+  const { getByRole } = render(<App />);
+  const checkbox = getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+//expect check box is checked after click
+
+test("Checkbox is checked should be false", () => {
+  const { getByRole } = render(<App />);
+  const checkbox = getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+});
+
+// check box is checked after click and is unchecked after second click
+
+test("Checkbox is checked should be false", () => {
+  const { getByRole } = render(<App />);
+  const checkbox = getByRole("checkbox");
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import {useState} from "react";
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [open, setOpen] = useState(true)
-
+  const [open, setOpen] = useState(true);
+  const [checked, setChecked] = useState(false);
 
   function classNames(...classes: any[]) {
     return classes.filter(Boolean).join(" ");
   }
 
-
-  
-
   return (
     <div className="container mx-auto">
       <header className="text-center p-5">
         <img src={logo} className="mx-auto" alt="logo" />
-        <p className="text-lg mt-5 py-6">
-          Welcome to our nice home page!
-        </p>
-        <button onClick={() => setOpen(!open)  }className={classNames(open ? "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded": "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded")  } >
- 
-        
+        <p className="text-lg mt-5 py-6">Welcome to our nice home page!</p>
+        <div className="flex justify-center">
+          <input
+            type="checkbox"
+            className="form-checkbox h-5 w-5 text-blue-600"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+          />
+          <label className="ml-2">Toggle Me</label>
+        </div>
+        <button
+          onClick={() => setOpen(!open)}
+          className={classNames(
+            open
+              ? "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              : "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          )}
+        >
           Test Button
         </button>
       </header>
     </div>
-
   );
 }
 
